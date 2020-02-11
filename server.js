a7090b1908c3393b4e1c3140d45a1dd16636637d
FrancisBilla <francis.billa.fb@gmail.com>
2020-02-11 15:03:51 +0000

    service for redirect

--- server.js
+++ server.js
@@ -1,44 +1,31 @@
-//const http = require('http');
-//const https = require('https');
-var http = require('follow-redirects').http;
 
+const req = require('request');
+const http = require('http');
 
-var server = http.createServer(function(request, response) {
-if (request.method == 'POST'){   
-    var body = '';
-    request.on('data', function(data){      
-        body += data;
-    });
-    request.on('end', function(){
-        try {
-            var post = JSON.parse(body);
-            console.log(post);         
-            response.write(post);
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            response.redirect(308,'hook.integromat.com/15ani2p9j8pub4s7hdwaippiqqrqobvp');            
-            response.end();
-            return;
-        }catch (err){
-            response.writeHead(500, {"Content-Type": "text/plain"});
-            response.write("Bad Post Data. Is yoour data a proper JSON?\n");
-            response.end();
-            return;
-        }
-    })
-    // .request({
-    //     followAllRedirects: true,
-    //     url: 'https://hook.integromat.com/15ani2p9j8pub4s7hdwaippiqqrqobvp'
-    // }, function (error, response, body){
-    //     if (!error){
-    //         console.log(response);
-    //     }
-    // }
-    // );
+ http.createServer(function(request, response) {    
+    if (request.method =='POST'){       
+         var body = '';
+        console.log('Status Code:', response.statusCode);
+        request.on('data', function(data){            
+             body += data;
+        });
+        request.on('end', function(){
+            var d = body;        
+        response.write(d);
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end();                       
+            var options = {
+                method:'post',
+                    url: 'https://hook.integromat.com/15ani2p9j8pub4s7hdwaippiqqrqobvp',
+                    json: true,
+                    headers: {
+                        'Content-Type': 'application/json',
+                       Accept: 'application/json'
+                   },
+                   body: {d}                                       
+                };
+                req.post(options);          
+        })    
 }
-});
-
-
-
-server.listen(8080)
-console.log("server started")
-
+}).listen(8080);
+console.log('server started')
