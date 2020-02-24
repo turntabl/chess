db4eab58dca0c2944338d2b973076c680581d69d
FrancisBilla <francis.billa.fb@gmail.com>
2020-02-24 19:22:05 +0000

    over

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const req = require("request");
-const http = require("http");
+const req = require('request');
+const http = require('http');
 
 http
   .createServer(function(request, response) {
-    if (request.method == "POST") {
-      var body = "";
-      console.log("Status Code:", response.statusCode);
-      request.on("data", function(data) {
+    if (request.method == 'POST') {
+      var body = '';
+      console.log('Status Code:', response.statusCode);
+      request.on('data', function(data) {
         body += data;
       });
-      request.on("end", function() {
+      request.on('end', function() {
         try {
           var incomingData = body;          
-          JSON.stringify(incomingData);
+        //  JSON.stringify(incomingData);
           response.write(incomingData);
-          response.writeHead(200, { "Content-Type": "text/plain" });
+          response.writeHead(200, { 'Content-Type': 'text/plain' });
           response.end();
           var options = {
-            method: "post",          
+            method: 'post',          
            url: process.env.URLintegromat,          
             json: true,
             headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json"
+              'Content-Type': 'application/json',
+              Accept: 'application/json'
             },
             body: { incomingData }
           };
           req.post(options);          
         } catch (err) {
-          response.writeHead(500, { "Content-Type": "text/plain" });
-          response.write("Bad Post Data. Is yoour data a proper JSON?\n");
+          response.writeHead(500, { 'Content-Type': 'text/plain' });
+          response.write('Bad Post Data. Is yoour data a proper JSON?\n');
           response.end();
           return;
         }
@@ -38,4 +38,4 @@ http
   })
   .listen(8080);
 
-console.log("server started");
+console.log('server started');
